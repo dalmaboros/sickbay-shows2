79cdc548a769bf264e340ffc0f4cd50e14370542
Dalma Boros <dalma.boros@gmail.com>
2017-07-05 10:54:19 -0500

    Venue show controller route and view

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -4,4 +4,24 @@ class VenuesController < ApplicationController
     @venues = Venue.order(:name)
     erb :'/venues/venues_index'
   end
+
+  # CREATE
+  get '/venues/new' do
+
+  end
+
+  post '/venues/new' do
+
+  end
+
+  # READ
+  get '/venues/:id' do
+    @venue = Venue.find_by(id: params[:id])
+    erb :'/venues/show_venue'
+  end
+
+  # EDIT
+
+  # DELETE
+
 end
