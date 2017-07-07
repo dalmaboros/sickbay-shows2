70c9791bf5eb464f5e2502c258c5baa71e500a4f
Dalma Boros <dalma.boros@gmail.com>
2017-07-07 09:07:32 -0500

    Show create and edit views only visible when logged in

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -61,33 +61,45 @@ class ShowsController < ApplicationController
 
   # EDIT
   get '/shows/:id/edit' do
-    @show = Show.find_by(id: params[:id])
-    erb :'/shows/edit_show'
+    if logged_in?
+      @show = Show.find_by(id: params[:id])
+      erb :'/shows/edit_show'
+    else
+      redirect to '/shows'
+    end
   end
 
   patch '/shows/:id' do
-    @show = Show.find_by(id: params[:id])
-    # binding.pry
-    @show.date = params[:date] if params[:date] != @show.date
-    @show.venue = Venue.find_or_create_by(name: params[:venue]) if params[:venue] != @show.venue.name
-    @show.url = params[:url] if params[:url] != @show.url && !params[:url].empty?
+    if logged_in?
+      @show = Show.find_by(id: params[:id])
+      # binding.pry
+      @show.date = params[:date] if params[:date] != @show.date
+      @show.venue = Venue.find_or_create_by(name: params[:venue]) if params[:venue] != @show.venue.name
+      @show.url = params[:url] if params[:url] != @show.url && !params[:url].empty?
 
-    @show.artists.clear
-    params[:artists].each do |artist|
-      if !artist.empty?
-        artist_object = Artist.find_or_create_by(name: artist)
-        @show.artists << artist_object
+      @show.artists.clear
+      params[:artists].each do |artist|
+        if !artist.empty?
+          artist_object = Artist.find_or_create_by(name: artist)
+          @show.artists << artist_object
+        end
       end
-    end
 
-    @show.save
-    redirect to "/shows/#{@show.id}"
+      @show.save
+      redirect to "/shows/#{@show.id}"
+    else
+      redirect to '/shows'
+    end
   end
 
   # DELETE
   delete '/shows/:id/delete' do
-    @show = Show.find_by(id: params[:id])
-    @show.destroy
-    redirect to '/'
+    if logged_in?
+      @show = Show.find_by(id: params[:id])
+      @show.destroy
+      redirect to '/'
+    else
+      redirect to '/shows'
+    end
   end
 end
