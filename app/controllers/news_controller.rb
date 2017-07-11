f82952d74a19a3272fc751877689073361f806ea
Dalma Boros <dalma.boros@gmail.com>
2017-07-11 12:53:05 -0500

    Updated news controller, and news views

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -32,14 +32,25 @@ class NewsController < ApplicationController
   # UPDATE
   get '/news/:id/edit' do
     if logged_in?
+      @news = News.find_by(id: params[:id])
       erb :'/news/edit_news'
     else
       redirect to '/news'
     end
   end
 
-  patch '/news/:id/edit' do
-    #
+  patch '/news/:id' do
+    if logged_in?
+      @news = News.find_by(id: params[:id])
+      @news.date = params[:date] if params[:date] != @news.date
+      @news.content = params[:content] if params[:content] != @news.content
+      @news.save
+      flash[:message] = "Successfully updated news item!"
+      redirect to "/news"
+    else
+      flash[:message] = "Could not update news item."
+      redirect to '/news/:id/edit'
+    end
   end
 
   delete '/news/:id/delete' do
