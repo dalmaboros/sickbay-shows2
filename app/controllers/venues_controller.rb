1898fc2a48f45a142979fff4d99d22e63d833cd8
Dalma Boros <dalma.boros@gmail.com>
2017-07-12 09:54:50 -0500

    Venues URLs now slugified

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -9,7 +9,6 @@ class VenuesController < ApplicationController
 
   # CREATE
   get '/venues/new' do
-    # binding.pry
     if logged_in?
       erb :'/venues/create_venue'
     else
@@ -25,7 +24,7 @@ class VenuesController < ApplicationController
       else
         @venue = Venue.create(name: params[:name])
         flash[:message] = "Successfully created venue!"
-        redirect to "venues/#{@venue.id}"
+        redirect to "venues/#{@venue.slug}"
       end
     else
       redirect to '/venues'
@@ -33,44 +32,44 @@ class VenuesController < ApplicationController
   end
 
   # READ
-  get '/venues/:id' do
-    @venue = Venue.find_by(id: params[:id])
+  get '/venues/:slug' do
+    @venue = Venue.find_by_slug(params[:slug])
     erb :'/venues/show_venue'
   end
 
   # EDIT
-  get '/venues/:id/edit' do
+  get '/venues/:slug/edit' do
     if logged_in?
-      @venue = Venue.find_by(id: params[:id])
+      @venue = Venue.find_by_slug(params[:slug])
       erb :'/venues/edit_venue'
     else
       redirect to '/venues'
     end
   end
 
-  patch '/venues/:id' do
+  patch '/venues/:slug' do
     if logged_in?
-      @venue = Venue.find_by(id: params[:id])
+      @venue = Venue.find_by_slug(params[:slug])
       @venue.name = params[:name] if params[:name] != @venue.name  && !params[:name].empty?
       @venue.save
       flash[:message] = "Successfully updated venue!"
-      redirect to "/venues/#{@venue.id}"
+      redirect to "/venues/#{@venue.slug}"
     else
       redirect to '/venues'
     end
   end
 
   # DELETE
-  delete '/venues/:id/delete' do
+  delete '/venues/:slug/delete' do
     @user = current_user
-    @venue = Venue.find_by(id: params[:id])
+    @venue = Venue.find_by_slug(params[:slug])
     if logged_in? && @user.authenticate(params[:password])
       @venue.destroy
       flash[:message] = "BALEETED!"
       redirect to '/venues'
     else
-      flash[:message] = "Incorrect password. Could not delete artist."
-      redirect to "/venues/#{@venue.id}/edit"
+      flash[:message] = "Incorrect password. Could not delete venue."
+      redirect to "/venues/#{@venue.slug}/edit"
     end
   end
 
