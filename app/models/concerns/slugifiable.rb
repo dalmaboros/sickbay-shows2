e7af97cd7f4f2d0104e15999b1944e9fcf26362c
Dalma Boros <dalma.boros@gmail.com>
2017-07-14 08:11:15 -0500

    Deleted comments, deleted backup database file, formatted backdoor.erb

--- app/models/concerns/slugifiable.rb
+++ app/models/concerns/slugifiable.rb
@@ -1,13 +1,13 @@
 module Slugifiable
 
-  module InstanceMethods #include
+  module InstanceMethods
     def slug
       I18n.transliterate(self.name.downcase.gsub(" ","-"))
 
     end
   end
 
-  module ClassMethods #extend
+  module ClassMethods
     def find_by_slug(slug)
       self.all.find {|s| s.slug == slug}
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-# Add seed data here. Seed your database with `rake db:seed`
+# Seed database with `rake db:seed`
 
 biergarten = Venue.create(name: "The Wurst Biergarten")
 
